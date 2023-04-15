24b49197c7d909641dd9af37cabca949e6f81453
syr <blossomy1d@gmail.com>
2023-04-15 14:34:01 +0900

    이미지 경로 재수정

--- Part13/13_2_3.js
+++ Part13/13_2_3.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded",
         let button = document.querySelector("input")
 
         let img = document.querySelector("img")
-        let img1 = "C:/instagram coding project image/etc 2.jpg"
-        let img2 = "C:/instagram coding project image/etc 3.jpg"
-        let img3 = "C:/instagram coding project image/etc 1.jpg"
+        let img1 = "C:/Users/User/Desktop/옐/취업/IDR시스템/스터디/instagram/image/etc 2.jpg"
+        let img2 = "C:/Users/User/Desktop/옐/취업/IDR시스템/스터디/instagram/image/etc 3.jpg"
+        let img3 = "C:/Users/User/Desktop/옐/취업/IDR시스템/스터디/instagram/image/etc 1.jpg"
 
         button.addEventListener("click",
             function(e) {
